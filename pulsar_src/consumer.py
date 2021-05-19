5d9bdd19fa31b2e4d521d1d94c7fe5e0bcd35ad6
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-19 16:03:58 +0200

    fixed indent syntax

--- pulsar_src/consumer.py
+++ pulsar_src/consumer.py
@@ -5,6 +5,13 @@ client = pulsar.Client('pulsar://localhost:6650')
 consumer = client.subscribe('DEtopic', subscription_name='DE-sub')
 # Display message received from producer
 msg = consumer.receive()
+
 try:
-print("Received message : '%s'" % msg.data())
-# Acknowledge for receiving the messa
+	print("Received message : '%s'" % msg.data())
+	# Acknowledge for receiving the message
+	consumer.acknowledge(msg)
+except:
+	consumer.negative_acknowledge(msg)
+
+# Destroy pulsar client
+client.close()

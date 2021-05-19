88b3c8b68384286ac2a2d8ec6e237c4c99e5b23a
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-19 18:57:22 +0200

    Fixes :)

--- pulsar_src/languageCounter.py
+++ pulsar_src/languageCounter.py
@@ -1,11 +1,16 @@
 from pulsar import Function
 
+
+'''
+
+sudo bin/pulsar-admin functions create --py ~/data_engineering_2_project/pulsar_src/languageCounter.py --classname languageCounter.languageCounter --inputs persistent://public/default/DEtopic --tenant public --namespace default
+
+'''
 class languageCounter(Function):
 	def __init__(self):
 		self.language_count_topic = "persistent://public/default/language_count"
 
 	def process(self, input, context):
-		
-		context.incr_counter(self, input, 1)
+		context.incr_counter(input, 1)
 		lang_count = (input, context.get_counter(input))
-		context.publish(self.language_count_topic, lang_count)
+		context.publish(self.language_count_topic, str(lang_count))

5a60bfb689140c384479813eacea32ab746a7986
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-24 16:35:55 +0200

    mongoDB insertion

--- pulsar_src/languageCounter.py
+++ pulsar_src/languageCounter.py
@@ -1,5 +1,5 @@
 from pulsar import Function
-
+import pymongo
 
 '''
 
@@ -12,5 +12,14 @@ class languageCounter(Function):
 
 	def process(self, input, context):
 		context.incr_counter(input, 1)
-		lang_count = (input, context.get_counter(input))
-		context.publish(self.language_count_topic, str(lang_count))
+		
+		client = pymongo.MongoClient("mongodb://localhost:27017/")
+		db = client["Github_statistics"]
+		col = db["language_count"]
+
+		key = {'language': input}
+		value = {'$set': {'count': count}}
+		col.update_one(key, value, upsert=True)
+		
+		#lang_count = (input, context.get_counter(input))
+		#context.publish(self.language_count_topic, str(lang_count))

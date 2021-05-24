5a0c6a38574d3286d95f1e7a03b633524dd399c7
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-24 13:49:45 +0200

    fix again :)

--- flask_src/app.py
+++ flask_src/app.py
@@ -19,22 +19,9 @@ app.config['DEBUG'] = True
 #turn the flask app into a socketio app
 socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
 
-#random number Generator Thread
+#Consumer thread
 thread = Thread()
 thread_stop_event = Event()
-
-def randomNumberGenerator():
-    """
-    Generate a random number every 1 second and emit to a socketio instance (broadcast)
-    Ideally to be run in a separate thread?
-    """
-    #infinite loop of magical random numbers
-    print("Making random numbers")
-    while not thread_stop_event.isSet():
-        number = round(random()*10, 3)
-        print(number)
-        socketio.emit('newnumber', {'number': number}, namespace='/test')
-        socketio.sleep(5)
 
 def pulsarStatistics():
 	while not thread_stop_event.isSet():
@@ -64,7 +51,6 @@ def test_connect():
     global thread
     print('Client connected')
 
-    #Start the random number generator thread only if the thread has not been started before.
     if not thread.isAlive():
         print("Starting Thread")
         thread = socketio.start_background_task(pulsarStatistics)
--- flask_src/static/js/app.js
+++ flask_src/static/js/app.js
@@ -5,26 +5,12 @@ $(document).ready(function(){
 
     var language_count = {}
 
+    //receive details from server
     socket.on('language_count', function(msg) {
         console.log("Received lang count: " + msg.language + " - " + msg.count);
         language_count[msg.language] = msg.count;
 
-        $('#log').html(language_count);
-    });
-
-    //receive details from server
-    socket.on('newnumber', function(msg) {
-        console.log("Received number" + msg.number);
-        //maintain a list of ten numbers
-        if (numbers_received.length >= 10){
-            numbers_received.shift()
-        }            
-        numbers_received.push(msg.number);
-        numbers_string = '';
-        for (var i = 0; i < numbers_received.length; i++){
-            numbers_string = numbers_string + '<p>' + numbers_received[i].toString() + '</p>';
-        }
-        $('#log').html(numbers_string);
+        $('#log').html(JSON.stringify(language_count));
     });
 
 });

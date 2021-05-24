96d73f48270ed338d45e0b9597a5a58ac8d5698d
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-24 18:03:07 +0200

    fix

--- pulsar_src/languageCounter.py
+++ pulsar_src/languageCounter.py
@@ -1,5 +1,5 @@
 from pulsar import Function
-import pymongo
+
 '''
 
 sudo bin/pulsar-admin functions create --py ~/data_engineering_2_project/pulsar_src/languageCounter.py --classname languageCounter.languageCounter --inputs persistent://public/default/DEtopic --tenant public --namespace default
@@ -11,12 +11,5 @@ class languageCounter(Function):
 
 	def process(self, input, context):
 		context.incr_counter(input, 1)
-		count = context.get_counter(input)
-		
-		client = pymongo.MongoClient("mongodb://localhost:27017/")
-		db = client["Github_statistics"]
-		col = db["language_count"]
-
-		key = {'language': input}
-		value = {'$set': {'count': count}}
-		col.update_one(key, value, upsert=True)
+		lang_count = (input, context.get_counter(input))
+		context.publish(self.language_count_topic, str(lang_count))

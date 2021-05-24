4e33169349bffbc39686d130c753144f50e6db39
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-24 16:54:39 +0200

    fix

--- pulsar_src/consumer.py
+++ pulsar_src/consumer.py
@@ -4,14 +4,19 @@ client = pulsar.Client('pulsar://localhost:6650')
 # Subscribe to a topic and subscription
 consumer = client.subscribe('language_count', subscription_name='lang_count-sub')
 # Display message received from producer
-msg = consumer.receive()
-
-try:
-	print("Received message : '%s'" % msg.data())
-	# Acknowledge for receiving the message
-	consumer.acknowledge(msg)
-except:
-	consumer.negative_acknowledge(msg)
+while True:
+	msg = consumer.receive()
+	try:
+		print("Received message : '%s'" % msg.data())
+		msg_tuple = msg.data().decode('utf-8')
+		msg_tuple = msg_tuple[1:]
+		msg_tuple = msg_tuple[:-1]
+		msg_tuple = tuple(msg_tuple.split(', '))
+		socketio.emit('language_count', {'language': msg_tuple[0], 'count': msg_tuple[1]}, namespace='/test')
+		# Acknowledge for receiving the message
+		consumer.acknowledge(msg)
+	except:
+		consumer.negative_acknowledge(msg)
 
 # Destroy pulsar client
 client.close()

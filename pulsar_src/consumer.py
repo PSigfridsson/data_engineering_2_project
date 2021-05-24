92b02fc772626d8efee16e4749aed4ba60a38cc8
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-24 18:04:43 +0200

    fix again

--- pulsar_src/consumer.py
+++ pulsar_src/consumer.py
@@ -1,4 +1,5 @@
 import pulsar
+import pymongo
 # Create a pulsar client by supplying ip address and port
 client = pulsar.Client('pulsar://localhost:6650')
 # Subscribe to a topic and subscription
@@ -13,6 +14,14 @@ try:
 	msg_tuple = msg_tuple[:-1]
 	msg_tuple = tuple(msg_tuple.split(', '))
 	print("Lang_count_tuple: ", msg_tuple)
+	
+	client = pymongo.MongoClient("mongodb://localhost:27017/")
+	db = client["Github_statistics"]
+	col = db["language_count"]
+
+	key = {'language': msg_tuple[0]}
+	value = {'$set': {'count': msg_tuple[1]}}
+	col.update_one(key, value, upsert=True)
 	# Acknowledge for receiving the message
 	consumer.acknowledge(msg)
 except:

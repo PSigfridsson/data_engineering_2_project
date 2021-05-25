804d9758675988b102a2ccb4e682c2efc99abb97
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-25 11:27:36 +0200

    flash changes

--- flask_src/static/js/app.js
+++ flask_src/static/js/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 
     $.getJSON("/_top10lang", function(data) {
-        console.log(data);
-        $('#top10lang').html(data);
+        console.log(data.top10);
+        $.each(data.top10, function(key,val) {
+            console.log("Key: "+key+" - Val: "+val);
+            $('#top10langtbody').append('<tr><td>'+key+'</td><td>'+val+'</td></tr>');
+        });
     });
 
 });

1f4c2cced1210218239f24f8499e9bda0fed178d
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-25 10:50:19 +0200

    fix

--- flask_src/app.py
+++ flask_src/app.py
@@ -1,64 +1,11 @@
-from flask_socketio import SocketIO, emit
 from flask import Flask, render_template, url_for, copy_current_request_context
-from random import random
-from time import sleep
-from threading import Thread, Event
-
-
-import pulsar
-from pulsar import ConsumerType
-# Create a pulsar client by supplying ip address and port
-client = pulsar.Client('pulsar://localhost:6650')
-# Subscribe to a topic and subscription
-consumer = client.subscribe('language_count', subscription_name='lang_count-sub', consumer_type=ConsumerType.Shared)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-app.config['DEBUG'] = True
-
-#turn the flask app into a socketio app
-socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
-
-#Consumer thread
-thread = Thread()
-thread_stop_event = Event()
-
-def pulsarStatistics():
-	while not thread_stop_event.isSet():
-		msg = consumer.receive()
-		try:
-			print("Received message : '%s'" % msg.data())
-			msg_tuple = msg.data().decode('utf-8')
-			msg_tuple = msg_tuple[1:]
-			msg_tuple = msg_tuple[:-1]
-			msg_tuple = tuple(msg_tuple.split(', '))
-			socketio.emit('language_count', {'language': msg_tuple[0], 'count': msg_tuple[1]}, namespace='/test')
-			# Acknowledge for receiving the message
-			consumer.acknowledge(msg)
-		except:
-			consumer.negative_acknowledge(msg)
-	# Destroy pulsar client
-	client.close()
 
 @app.route('/')
 def index():
     #only by sending this page first will the client be connected to the socketio instance
     return render_template('index.html')
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    # need visibility of the global thread object
-    global thread
-    print('Client connected')
-
-    if not thread.isAlive():
-        print("Starting Thread")
-        thread = socketio.start_background_task(pulsarStatistics)
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
-
-
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run(host='0.0.0.0', port=5000)
--- flask_src/static/js/app.js
+++ flask_src/static/js/app.js
@@ -1,16 +1,3 @@
 $(document).ready(function(){
-    //connect to the socket server.
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
-    var numbers_received = [];
-
-    var language_count = {}
-
-    //receive details from server
-    socket.on('language_count', function(msg) {
-        console.log("Received lang count: " + msg.language + " - " + msg.count);
-        language_count[msg.language] = msg.count;
-
-        $('#log').html(JSON.stringify(language_count));
-    });
-
+    console.log("Hello world :)");
 });

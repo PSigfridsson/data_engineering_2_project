6085da492c2582e459ba6cb37dd63157c1394999
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-25 11:30:42 +0200

    flask

--- flask_src/static/js/app.js
+++ flask_src/static/js/app.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     $.getJSON("/_top10lang", function(data) {
         console.log(data.top10);
         $.each(data.top10, function(key,val) {
-            console.log("Key: "+key+" - Val: "+val);
-            $('#top10langtbody').append('<tr><td>'+key+'</td><td>'+val+'</td></tr>');
+            console.log("Val0: "+val[0]+" - Val1: "+val[1]);
+            $('#top10langtbody').append('<tr><td>'+val[0]+'</td><td>'+val[1]+'</td></tr>');
         });
     });
 

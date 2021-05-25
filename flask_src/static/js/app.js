68eb5d54a4966872c2240a6a508fa962c33b3976
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-25 12:08:56 +0200

    fix

--- flask_src/static/js/app.js
+++ flask_src/static/js/app.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         console.log(data.top10);
         $.each(data.top10, function(key,val) {
             console.log("Val0: "+val[0]+" - Val1: "+val[1]);
-            $('#top10langtbody').prepend('<tr><td>'+val[0]+'</td><td>'+val[1]+'</td></tr>');
+            $('#top10langtbody').append('<tr><td>'+val[0]+'</td><td>'+val[1]+'</td></tr>');
         });
     });
 

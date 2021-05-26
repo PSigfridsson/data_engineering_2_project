eaefc407e413b8fc5335f607c2c8c37e9f520a71
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-26 11:12:55 +0200

    fix

--- pulsar_src/unittestCounter.py
+++ pulsar_src/unittestCounter.py
@@ -1,6 +1,6 @@
 from pulsar import Function
-#import requests
-#import json
+import requests
+import json
 
 '''
 sudo bin/pulsar-admin functions create --py ~/data_engineering_2_project/pulsar_src/unittestCounter.py --classname unittestCounter.unittestCounter --inputs persistent://public/default/q3-input --tenant public --namespace default
@@ -15,8 +15,7 @@ class unittestCounter(Function):
         token = "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"
 
         input_split = input.split(' ')
-        context.publish(self.unittest, '{}'.format(input_split[0]))
-        #files = requests.get('{}'.format(input_split[1] + '/contents'), auth=(username, token))
-        # for j in range(len(files.json())):
-        #     if 'test' in files.json()[j]['name']:
-        #         context.publish(self.unittest, '{}'.format(input_split[0]))
+        files = requests.get('{}'.format(input_split[1] + '/contents'), auth=(username, token))
+        for j in range(len(files.json())):
+            if 'test' in files.json()[j]['name']:
+                context.publish(self.unittest, '{}'.format(input_split[0]))

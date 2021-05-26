8976380ab1e60f5c6f7baf39ae33f90f38ec027e
Psigfridsson <petter.sigfridsson@gmail.com>
2021-05-26 11:22:41 +0200

    fix indent

--- pulsar_src/consumerUnitTest.py
+++ pulsar_src/consumerUnitTest.py
@@ -6,26 +6,26 @@ client = pulsar.Client('pulsar://localhost:6650')
 consumer = client.subscribe('q3-output', subscription_name='DE-sub', consumer_type=ConsumerType.Shared)
 
 while True:
-    msg = consumer.receive()
-    try:
-    	print("Received message : '%s'" % msg.data())
-    	msg_tuple = msg.data().decode('utf-8')
-    	msg_tuple = msg_tuple[1:]
-    	msg_tuple = msg_tuple[:-1]
-    	msg_tuple = tuple(msg_tuple.split(', '))
-    	input_tuple = (msg_tuple[0][1:][:-1], int(msg_tuple[1]))
-    	print("unit_count_tuple: ", input_tuple)
+	msg = consumer.receive()
+	try:
+		print("Received message : '%s'" % msg.data())
+		msg_tuple = msg.data().decode('utf-8')
+		msg_tuple = msg_tuple[1:]
+		msg_tuple = msg_tuple[:-1]
+		msg_tuple = tuple(msg_tuple.split(', '))
+		input_tuple = (msg_tuple[0][1:][:-1], int(msg_tuple[1]))
+		print("unit_count_tuple: ", input_tuple)
 
-    	mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
-    	db = mongoClient["Github_statistics"]
-    	col = db["language_count"]
+		mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
+		db = mongoClient["Github_statistics"]
+		col = db["language_count"]
 
-    	key = {'language': input_tuple[0]}
-    	value = {'$set': {'count': input_tuple[1]}}
-    	col.update_one(key, value, upsert=True)
-        consumer.acknowledge(msg)
-    except:
-        consumer.negative_acknowledge(msg)
-        
+		key = {'language': input_tuple[0]}
+		value = {'$set': {'count': input_tuple[1]}}
+		col.update_one(key, value, upsert=True)
+		consumer.acknowledge(msg)
+	except:
+		consumer.negative_acknowledge(msg)
+		
 # Destroy pulsar client
 client.close()

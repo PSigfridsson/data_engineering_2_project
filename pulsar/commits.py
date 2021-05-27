9cb6e27194c905e587f3da10d8607e99557d18f7
elenafilonova <ev.filonova@gmail.com>
2021-05-27 18:19:19 +0000

    Updated pulsar function for commits count

--- pulsar/commits.py
+++ pulsar/commits.py
@@ -2,9 +2,6 @@
 
 from pulsar import Function
 import requests
-import os
-import json
-import time
 import regex as re
 import random
 
@@ -12,11 +9,13 @@ class commitsCounter(Function):
     def __init__(self):
         self.commits = "persistent://public/default/q2-output"
     def process(self, input, context):
-        tokens = {'elenafilonova': 'ccec9e791ed01388009a380162f8ad5a9feb9b53', 'WSandkvist': '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', 'AlexisTubulekas': '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'}
+        tokens = {'elenafilonova': 'ccec9e791ed01388009a380162f8ad5a9feb9b53', 'WSandkvist': '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', 'AlexisTubulekas': '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', 'psigfridsson': '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'}
         username = random.choice(list(tokens))
         token = tokens[username]
         input_split = input.split(' ')
-        commits = re.search('\d+$', requests.get('{}'.format(input_split[1] + '/commits?per_page=1'), auth=(username, token)).links['last']['url']).group()
+        try:
+            commits = re.search('\d+$', requests.get('{}'.format(input_split[1] + '/commits?per_page=1'), auth=(username, token)).links['last']['url']).group()
+        except:
+            commits = 0
         context.publish(self.commits, '{} {}'.format(input_split[0], commits))
-        
         
